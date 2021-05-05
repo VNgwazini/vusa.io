91e572d482f4fcd9cdff50cea170b7c6de24dd4e
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2021-05-04 20:37:31 -0700

    Added margin to maintenance page.

--- vusa.io/src/App.js
+++ vusa.io/src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:vusumuzi.ngwazini@gmail.com">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
-        <p>&mdash; The Team</p>
+        <p className="App-p">Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:vusumuzi.ngwazini@gmail.com">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+        <p className="App-p">&mdash; The Team</p>
         <a
           className="App-link"
           href="https://www.linkedin.com/in/vusumuzi-ngwazini/"
