a2a080f41f5d8c74d2beef5b4cc6861ec3ff7971
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2021-05-05 12:55:32 -0700

    Decreased paragraph margin and added image to manifest.

--- vusa.io/src/App.js
+++ vusa.io/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo}  alt="logo" />{/*className="App-logo"*/}
-        <p class="mx-4 mb-0">Sorry for the inconvenience but I&rsquo;m performing some maintenance at the moment. If you need to you can always <a href="mailto:vusumuzi.ngwazini@gmail.com">contact me</a>, otherwise I&rsquo;ll be back online shortly!</p>
+        <p class="mb-0">Sorry for the inconvenience but I&rsquo;m performing some maintenance at the moment. If you need to you can always <a href="mailto:vusumuzi.ngwazini@gmail.com">contact me</a>, otherwise I&rsquo;ll be back online shortly!</p>
         <a
           className="App-link"
           href="https://www.linkedin.com/in/vusumuzi-ngwazini/"
