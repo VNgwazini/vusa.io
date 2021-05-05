9cd262cc91772e1e668a312aab1e18686ffc51ea
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2021-05-05 12:17:44 -0700

    Added custom images to maintenance site.

--- vusa.io/src/App.js
+++ vusa.io/src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
+import logo from './logo.webp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p className="App-p">Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:vusumuzi.ngwazini@gmail.com">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
-        <p className="App-p">&mdash; The Team</p>
+        <img src={logo}  alt="logo" />{/*className="App-logo"*/}
+        <p class="mx-4 mb-0">Sorry for the inconvenience but I&rsquo;m performing some maintenance at the moment. If you need to you can always <a href="mailto:vusumuzi.ngwazini@gmail.com">contact me</a>, otherwise I&rsquo;ll be back online shortly!</p>
         <a
           className="App-link"
           href="https://www.linkedin.com/in/vusumuzi-ngwazini/"
