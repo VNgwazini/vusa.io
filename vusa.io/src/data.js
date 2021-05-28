b168a046ab4fb0164b5345ee620721b2fc211d03
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2021-05-27 18:04:20 -0700

    #1 Added portfolio list component that dynamically populates from new data file.

---
+++ vusa.io/src/data.js
@@ -0,0 +1,80 @@
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: "Rental Property App",
+    img:
+      "assets/RentBarkerPreview.png",
+  },
+  {
+    id: 2,
+    title: "Healthcare App",
+    img:
+      "assets/WalaPreview.png",
+  },
+  {
+    id: 3,
+    title: "Android Memory Game",
+    img:
+      "assets/Khumbula_feature_graphic.png",
+  }
+];
+
+export const webPortfolio = [
+  {
+    id: 1,
+    title: "Web Rental Property App",
+    img:
+      "assets/RentBarkerPreview.png",
+  },
+  {
+    id: 2,
+    title: "Web Healthcare App",
+    img:
+      "assets/WalaPreview.png",
+  }
+];
+
+export const mobilePortfolio = [
+  {
+    id: 1,
+    title: "Mobile Android Memory Game",
+    img:
+      "assets/Khumbula_feature_graphic.png",
+  }
+];
+
+export const designPortfolio = [
+  {
+    id: 1,
+    title: "Design Healthcare App",
+    img:
+      "assets/WalaPreview.png",
+  },
+  {
+    id: 2,
+    title: "Design Android Memory Game",
+    img:
+      "assets/Khumbula_feature_graphic.png",
+  }
+];
+
+export const contentPortfolio = [
+  {
+    id: 1,
+    title: "Content Rental Property App",
+    img:
+      "assets/RentBarkerPreview.png",
+  },
+  {
+    id: 2,
+    title: "Content Healthcare App",
+    img:
+      "assets/WalaPreview.png",
+  },
+  {
+    id: 3,
+    title: "Content Android Memory Game",
+    img:
+      "assets/Khumbula_feature_graphic.png",
+  }
+];
