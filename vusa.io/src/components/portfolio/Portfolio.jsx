ee1e8e9f38f05bd93de38c267f3c95414c9b3df2
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2021-06-01 10:17:29 -0700

    #1 Added links to carousel items.

--- vusa.io/src/components/portfolio/Portfolio.jsx
+++ vusa.io/src/components/portfolio/Portfolio.jsx
@@ -13,47 +13,53 @@ export default function Portfolio() {
     <Container className="portfolio" id="portfolio" fluid="true">
       <Row>
         <Col>
-          <Carousel variant="light">
+          <Carousel variant="light">\
             <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="assets/Khumbula_feature_graphic.png"
-                alt="Android Game"
-                style={{
-                  height: "100vh",
-                  width: "100vw"
-                }}
-              />
+              <a href="https://play.google.com/store/apps/details?id=com.vusa.mymemory">
+                <img
+                  className="d-block w-100"
+                  src="assets/Khumbula_feature_graphic.png"
+                  alt="Android Game"
+                  style={{
+                    height: "100vh",
+                    width: "100vw"
+                  }}
+                />
+              </a>
               <Carousel.Caption>
                 <h5>Android Game</h5>
                 <p>A matching game with 3 difficulty levels.</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="assets/WalaPreview.png"
-                alt="Healthcare App"
-                style={{
-                  height: "100vh",
-                  width: "100vw"
-                }}
-              />
+              <a href="https://github.com/VNgwazini/Healthcare-App">
+                <img
+                  className="d-block w-100"
+                  src="assets/WalaPreview.png"
+                  alt="Healthcare App"
+                  style={{
+                    height: "100vh",
+                    width: "100vw"
+                  }}
+                />
+              </a>
               <Carousel.Caption>
                 <h5>Healthcare App</h5>
                 <p>A one-page app for managing blood inventory and requests across a network of blood banks.</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="assets/RentBarkerPreview.png"
-                alt="Rental Property App"
-                style={{
-                  height: "100vh",
-                  width: "100vw"
-                }}
-              />
+              <a href="https://github.com/VNgwazini/Rental-Property-App">
+                <img
+                  className="d-block w-100"
+                  src="assets/RentBarkerPreview.png"
+                  alt="Rental Property App"
+                  style={{
+                    height: "100vh",
+                    width: "100vw"
+                  }}
+                />
+              </a>
               <Carousel.Caption>
                 <h5>Rental Property App</h5>
                 <p>A preference based app for finding rental properties.</p>
