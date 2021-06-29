5a10f65173b388ea93f2d904d3199572ad1072ea
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2021-06-29 02:42:29 -0700

    Made projects component dynamic by adding props from data file.

--- vusa.io/src/components/projects/Projects.jsx
+++ vusa.io/src/components/projects/Projects.jsx
@@ -1,13 +1,20 @@
 import "./projects.scss";
+import { useState } from "react";
 import { 
   Row,
   Col,
   Container,
-  Card
 } from 'react-bootstrap/'
-import {Mail, LinkedIn, GitHub, Twitter} from "@material-ui/icons"
+
+import ProjectListItem from "../projectListItem/ProjectListItem";
+import {
+  projectData
+} from "../../data";
 
 export default function Projects() {
+
+  const [data, setData] = useState(projectData);
+
   return (
     <Container className="projects pt-4 content-container" id="projects" fluid="true">
       <Row>
@@ -16,72 +23,9 @@ export default function Projects() {
         </Col>
       </Row>
       <Row className="d-flex justify-content-center text-center">
-      <Col className="d-flex justify-content-center py-4" sm="4">
-        <a 
-          href="https://play.google.com/store/apps/details?id=com.vusa.mymemory"
-          target="_blank" rel="noreferrer"
-        >
-        <Card className="shadow card-size mx-4 my-4">
-            <Card.Img 
-              src="assets/Khumbula_feature_graphic.png"
-              alt="Khumbula"
-              className="img-thumbnail"
-              style={{border: "transparent"}}
-            />
-            <Card.Title>Khumbula: Memory Game</Card.Title>
-            <Card.Body>
-              <Card.Text>
-                An image matching game for Android with 3 difficulty levels.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer className="text-muted">2021 · Android App Development</Card.Footer>
-          </Card>
-        </a>
-        </Col>
-        <Col className="d-flex justify-content-center py-4" sm="4">
-        <a 
-          href="https://demo-rental-property-app.vusa.io/"
-          target="_blank" rel="noreferrer"
-        >
-        <Card className="shadow card-size mx-4 my-4" sm="4">
-            <Card.Img 
-              src="assets/RentBarkerPreview.png"
-              alt="Rental"
-              className="img-thumbnail"
-              style={{border: "transparent"}}
-            />
-            <Card.Title>Rental Property App</Card.Title>
-            <Card.Body>
-              <Card.Text>
-                A react app for finding rental properties based on your preferences.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer className="text-muted">2020 · Web App Development</Card.Footer>
-          </Card>
-        </a>
-        </Col>
-        <Col className="d-flex justify-content-center py-4">
-        <a 
-          href="https://demo-healthcare-app.vusa.io/"
-          target="_blank" rel="noreferrer"
-        >
-        <Card className="shadow card-size mx-4 my-4">
-            <Card.Img 
-              src="assets/WalaPreview.png"
-              alt="Healthcare"
-              className="img-thumbnail"
-              style={{border: "transparent"}}
-            />
-            <Card.Title>Healthcare App</Card.Title>
-            <Card.Body>
-              <Card.Text>
-                An app for managing blood inventory between multiple blood banks.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer className="text-muted">2020 · Web App Development</Card.Footer>
-          </Card>
-        </a>
-        </Col>
+        {data.map((item) => (
+          <ProjectListItem item={item}/>
+        ))}
       </Row>
     </Container>
   )
---
+++ vusa.io/src/data.js
@@ -0,0 +1,29 @@
+export const projectData = [
+  {
+    id: 1,
+    title: "Khumbula: Memory Game",
+    link: "https://play.google.com/store/apps/details?id=com.vusa.mymemory",
+    text: "An image matching game for Android with 3 difficulty levels.",
+    footer: "2021 · Android App Development",
+    imgSrc: "assets/Khumbula_feature_graphic.png",
+    imgAlt: "Khumbula",
+  },
+  {
+    id: 2,
+    title: "Rental Property App",
+    link: "https://demo-rental-property-app.vusa.io/",
+    text: "A react app for finding rental properties based on your preferences.",
+    footer: "2020 · Web App Development",
+    imgSrc: "assets/RentBarkerPreview.png",
+    imgAlt: "Rental",
+  },
+  {
+    id: 3,
+    title: "Healthcare App",
+    link: "https://demo-healthcare-app.vusa.io/",
+    text: "An app for managing blood inventory between multiple blood banks.",
+    footer: "2020 · Web App Development",
+    imgSrc: "assets/WalaPreview.png",
+    imgAlt: "Healthcare",
+  }
+];
