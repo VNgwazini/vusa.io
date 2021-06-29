57fff07a6ff6a08940128adc7b94d6d399a8493a
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2021-06-29 01:05:03 -0700

    Added typing effect.

--- vusa.io/src/components/about/About.jsx
+++ vusa.io/src/components/about/About.jsx
@@ -1,4 +1,6 @@
 import "./about.scss";
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
 import { 
   Row,
   Col,
@@ -8,6 +10,18 @@ import {
 import {Mail, LinkedIn, GitHub, Twitter} from "@material-ui/icons"
 
 export default function About() {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, { 
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:  60,
+      strings: ['Software Engineer','Back-end Developer', 'Front-end Developer'],
+    })
+  }, [])
+
   return (
     <Container className="about content-container" id="about" fluid="true">
       <Row className="h-50">
@@ -25,6 +39,9 @@ export default function About() {
         <Col className="d-flex justify-content-center align-items-center">
           <Card style={{  border: "transparent" }}>
             <Card.Title className="text-center pt-2">Vusumuzi Ngwazini</Card.Title>
+            <Card.Subtitle className="text-center text-muted">
+              Freelance <span className="typing" ref={textRef}></span>
+            </Card.Subtitle>
             <Card.Body className="text-center">
               <Card.Text>
                 Vusumuzi is a software engineer, technology enthusiast, and is active in the technology startup community.
