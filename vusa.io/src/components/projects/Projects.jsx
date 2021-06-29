0b95dca2b4edf5b99e8b86889193a5bfbaa8f546
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2021-06-29 00:19:07 -0700

    Added projects component.

---
+++ vusa.io/src/components/projects/Projects.jsx
@@ -0,0 +1,88 @@
+import "./projects.scss";
+import { 
+  Row,
+  Col,
+  Container,
+  Card
+} from 'react-bootstrap/'
+import {Mail, LinkedIn, GitHub, Twitter} from "@material-ui/icons"
+
+export default function Projects() {
+  return (
+    <Container className="projects pt-4" id="content" fluid="true">
+      <Row>
+         <Col className="d-flex justify-content-center py-4">
+          <h5 className="px-4 text-left">Software Engineering Portfolio</h5>
+        </Col>
+      </Row>
+      <Row className="d-flex justify-content-center text-center">
+      <Col className="d-flex justify-content-center py-4">
+        <a 
+          href="https://play.google.com/store/apps/details?id=com.vusa.mymemory"
+          target="_blank" rel="noreferrer"
+        >
+        <Card className="shadow card-size mx-4 my-4">
+            <Card.Img 
+              src="assets/Khumbula_feature_graphic.png"
+              alt="Khumbula"
+              className="img-thumbnail"
+              style={{border: "transparent"}}
+            />
+            <Card.Title>Khumbula: Memory Game</Card.Title>
+            <Card.Body>
+              <Card.Text>
+                An image matching game for Android with 3 difficulty levels.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer className="text-muted">2021 · Android App Development</Card.Footer>
+          </Card>
+        </a>
+        </Col>
+        <Col className="d-flex justify-content-center py-4">
+        <a 
+          href="https://demo-rental-property-app.vusa.io/"
+          target="_blank" rel="noreferrer"
+        >
+        <Card className="shadow card-size mx-4 my-4">
+            <Card.Img 
+              src="assets/RentBarkerPreview.png"
+              alt="Rental"
+              className="img-thumbnail"
+              style={{border: "transparent"}}
+            />
+            <Card.Title>Rental Property App</Card.Title>
+            <Card.Body>
+              <Card.Text>
+                A preference based app for finding rental properties.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer className="text-muted">2020 · Web App Development</Card.Footer>
+          </Card>
+        </a>
+        </Col>
+        <Col className="d-flex justify-content-center py-4">
+        <a 
+          href="https://demo-healthcare-app.vusa.io/"
+          target="_blank" rel="noreferrer"
+        >
+        <Card className="shadow card-size mx-4 my-4">
+            <Card.Img 
+              src="assets/WalaPreview.png"
+              alt="Healthcare"
+              className="img-thumbnail"
+              style={{border: "transparent"}}
+            />
+            <Card.Title>Healthcare App</Card.Title>
+            <Card.Body>
+              <Card.Text>
+                An app for managing blood inventory between multiple blood banks.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer className="text-muted">2020 · Web App Development</Card.Footer>
+          </Card>
+        </a>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
--- vusa.io/src/index.jsx
+++ vusa.io/src/index.jsx
@@ -4,6 +4,7 @@ import './index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Topbar from './components/topbar/Topbar';
+import Projects from './components/projects/Projects';
 import About from './components/about/About';
 import Footer from './components/footer/Footer';
 
@@ -12,6 +13,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Topbar />
     <About />
+    <Projects/>
     <Footer />
   </React.StrictMode>,
   document.getElementById('root')
