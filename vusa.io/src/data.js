2b7f8a56788321449e90e7c1e4a6f36c18706742
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2021-06-29 17:52:25 -0700

    Added resume experience to json file.

--- vusa.io/src/data.js
+++ vusa.io/src/data.js
@@ -27,3 +27,186 @@ export const projectData = [
     imgAlt: "Healthcare",
   }
 ];
+
+
+export const experienceData = [
+  {
+    category: "PROFESSIONAL EXPERIENCE",
+    jobs: [
+      {id: 1,
+        company: "Bhala - African Keyboard",
+        position: "Software Engineer, Android",
+        duration: "March 2021 – April 2021",
+        about: "Bhala is an African startup featuring a multilingual keyboard that learns your writing style and makes writing in African languages faster by leveraging artificial intelligence designed for African languages.",
+        responsibilities: [
+        "Migrating underlying legacy library to Kotlin to increase efficiency and compatibility.",
+        "Developing API and features for published, customer-facing android app utilizing Kotlin Firebase."
+        ]
+      },
+      {id: 2,
+        company: "Wala Digital Health",
+        position: "Software Engineer, Full Stack",
+        duration: "October 2020 – February 2021",
+        about: "Harvard and MIT alumni-founded startup featuring an integrated digital platform for easy, quick blood supply and sharing critical for life-saving transfusions.",
+        responsibilities: [
+        "Developed a single-page application for online blood management through the software development life cycle, utilizing the React framework on the front end, and MongoDB and Strapi on the back end.",
+        "Built core features & functionalities for minimal viable product based on client needs and feedback",
+        "Unit testing and REST API endpoint testing using Jest JavaScript testing framework.",
+        "Software and system design, documentation, and integration."
+        ]
+      },
+      {id: 3,
+        company: "Bantucracy",
+        position: "Software Engineer, Full Stack",
+        duration: "August 2020 – November 2020",
+        about: "Boston research-based tech & tech policy startup company with a strong focus on ubuntu ethics, technology and human rights.",
+        responsibilities: [
+        "Built the landing page and main features of a minimal viable product from concept to deployment as specified by the UI/UX team and customer.",
+        "Developed front-end components and assisted in back-end development of RESTful web service utilizing Bootstrap 4, React, Node.js, Express.js, MongoDB, Next.js, Material-UI, Strapi, and Docker."
+        ]
+      },
+      {id: 4,
+        company: "Lockheed Martin Aeronautics",
+        position: "Software Engineer, Backend",
+        duration: "July 2019 – August 2020",
+        about: "",
+        responsibilities: [
+        "Large-scale enterprise software design and feature development utilizing C++, Java, and Python in both Linux and Windows virtual environments.",
+        "Graphical user interface tool development with Java Swing, JavaFX toolkits, & C++ QT Framework.",
+        "Continuous integration pipelining, test automation, and version control using Bitbucket, Jira, & Jenkins.",
+        "Coding sprints, team standup meetings, and peer code reviews leveraging Scrum Agile Framework."
+        ]
+      },
+      {id: 5,
+        company: "Oak Ridge National Laboratory",
+        position: "Intern, Machine Learning",
+        duration: "Spring 2019",
+        about: "",
+        responsibilities: [
+        "Utilized high-speed sensor data to train neural networks to predict the amplitude of aperiodic signals.",
+        "Visualized engine and neural network performance data to serve as a baseline for future development."
+        ]
+      },
+      {id: 6,
+        company: "Florida Hospital, Advent Health",
+        position: "Intern, Backend",
+        duration: "Summer 2018",
+        about: "",
+        responsibilities: [
+        "Developed SQL based program (Cerner Coding Language) to mine the Cerner API for data on mothers and their newborns to track hospital acquired infections (HAI) and cross-contamination rates.",
+        "Visualized and analyzed data with Power BI and Oracle BI for Clinical Applications team."
+        ]
+      },
+      {id: 7,
+        company: "Information Consultants",
+        position: "Intern",
+        duration: "Fall 2017 – Spring 2018",
+        about: "",
+        responsibilities: [
+        "Built input forms and designed back-end workflows to automate business processes and push data to client databases.",
+        ]
+      },
+      {id: 8,
+        company: "Information Inclusion Institute",
+        position: "Intern",
+        duration: "Summer 2017",
+        about: "",
+        responsibilities: [
+        "Utilized Python to mine for JSON data on the Twitter API & performed analysis utilizing R-Studio to measure the impact of a major social event through social media platforms in real time.",
+        ]
+      }
+    ]
+  },
+  {
+    category: "PERSONAL PROJECTS",
+    jobs: [
+      {id: 1,
+        company: "Khumbula - Image Matching Game",
+        position: "Software Engineer, Android",
+        duration: "January 2021 – Currently",
+        about: "Khumbula, meaning 'remember' in the Zulu language, is an open-source African themed matching game that serves the goal of sharpening our user’s short-term memory by finding pairs of tiles in the shortest number of moves.",
+        responsibilities: [
+        "Developing main activities, & XML layouts for our published app utilizing Kotlin & Android Studio.",
+        ]
+      }
+    ]
+  },
+  {
+    category: "LEADERSHIP EXPERIENCE",
+    jobs: [
+      {id: 1,
+        company: "National Society of Black Engineers",
+        position: "President",
+        duration: "Fall 2018 – Spring 2019",
+        about: "",
+        responsibilities: [
+          "Tripled volunteering efforts, community involvement, and chapter membership by 266%.",
+          "Secured sponsor for chapter, doubled fundraising efforts, and raised chapter budget by 100%.",
+          "Launched long-term partnership with STEM Lab at local Boys & Girls club for engineering mentorship program.",
+        ]
+      },
+      {id: 2,
+        company: "African Student Association",
+        position: "Vice-President",
+        duration: "Fall 2018 – Spring 2019",
+        about: "",
+        responsibilities: [
+          "Doubled volunteering efforts, community involvement, and chapter membership by 100%.",
+          "Collaborated with local universities and shops to secure sponsors, authentic food vendors, fashion designers, and performers for our annual African Culture Showcase.",
+        ]
+      }
+    ]
+  },
+  {
+    category: "RESEARCH EXPERIENCE",
+    jobs: [
+      {id: 1,
+        company: "Information Inclusion Institute",
+        position: "Researcher",
+        duration: "Summer 2017 – Summer 2018",
+        about: "iConference 2018 – Research Paper & Poster",
+        responsibilities: [
+          "Cognitively Salient Wayfinding Aids for Emergency First Responders",
+        ]
+      },
+      {id: 2,
+        company: "Information Inclusion Institute",
+        position: "Researcher",
+        duration: "Summer 2018 – Winter 2018",
+        about: "iConference 2019 – Research Paper & Poster",
+        responsibilities: [
+          "Firefighters’ Strategies for Processing Spatial Information During Emergency Rescue Searches",
+        ]
+      }
+    ]
+  },
+  {
+    category: "ACADEMIC ACHIEVEMENTS",
+    jobs: [
+      {id: 1,
+        company: "Alabama Association of Colleges and Employers (AACE)",
+        position: "Outstanding Intern of the Year",
+        duration: "Spring 2019",
+        about: "Recognizes the AACE college member who, through specific achievement(s) or combined contributions to AACE, has exemplified outstanding professionalism, commitment, and cooperation.",
+        responsibilities: [
+        ]
+      },
+      {id: 2,
+        company: "Oakwood University",
+        position: "Dean’s List",
+        duration: "Spring 2016 – Fall 2019",
+        about: "",
+        responsibilities: [
+        ]
+      },
+      {id: 3,
+        company: "Oakwood University",
+        position: "Dr. Carmen Wright & Chandler Foundation Scholarship",
+        duration: "Spring 2018",
+        about: "",
+        responsibilities: [
+        ]
+      }
+    ]
+  },
+];
