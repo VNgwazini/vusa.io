0a8de78e65d42d30ec91ebf349d3a8e3b98a4f59
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2022-01-18 12:36:29 -0800

    Footer and topbar now match navy blue theme.

--- vusa.io/src/components/footer/Footer.jsx
+++ vusa.io/src/components/footer/Footer.jsx
@@ -4,9 +4,9 @@ import {Mail, LinkedIn, GitHub, Twitter, Nature} from "@material-ui/icons"
 export default function Footer() {
   return (
     <div className="footer d-flex align-items-center justify-content-between px-1 py-4">
-      <span className="d-flex justify-content-start">Vusumuzi Ngwazini © 2021</span>
+      <span className="d-flex justify-content-start px-2">Vusumuzi Ngwazini © 2021</span>
       <span className="d-flex justify-content-between">
-        <a target="_blank" href="https://www.linkedin.com/in/vusumuzi-ngwazini/" rel="noreferrer"><LinkedIn className="mx-1"/></a>
+        <a target="_blank" href="https://www.linkedin.com/in/vusumuzi-ngwazini/" rel="noreferrer"><LinkedIn className="mx-1"style={{fill: "navy"}}/></a>
         <a target="_blank" href="https://github.com/VNgwazini" rel="noreferrer"><GitHub className="mx-1"/></a>
         <a target="_blank" href="https://twitter.com/VNgwazini" rel="noreferrer"><Twitter className="mx-1"/></a>
         <a target="_blank" href="mailto:vusumuzi.ngwazini@gmail.com" rel="noreferrer"><Mail className="mx-1"/></a>
