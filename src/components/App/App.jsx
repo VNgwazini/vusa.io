6d0492bdfa350627a8e15ea41fa7a18385a0cd84
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2022-04-29 16:57:55 -0700

    #29 home components now moved to accordion.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,12 @@ import Projects from '../../components/projects/Projects';
 import Experience from '../../components/experience/Experience';
 import Resume from '../../components/resume/Resume';
 import FadeIn from 'react-fade-in';
+import { 
+  Container,
+  Row,
+  Col,
+  Accordion,
+} from 'react-bootstrap/'
 
 import Home from "../home/Home";
 
@@ -14,9 +20,32 @@ export default function App() {
     <FadeIn>
       <Home/>
       {/* <Payment/> */}
-      <Projects/>
-      <Experience/>
-      <Resume/>
+      <Container className="options content-container mb-4 pt-4" id="options" fluid="true">
+        <Row>
+          <Col>
+            <Accordion className="px-4">
+              <Accordion.Item eventKey="0">
+                <Accordion.Header className="d-flex justify-content-center">Software Engineering Portfolio</Accordion.Header>
+                <Accordion.Body>
+                  <Projects/>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>Work Experience</Accordion.Header>
+                <Accordion.Body>
+                  <Experience/>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="2">
+                <Accordion.Header>Resume</Accordion.Header>
+                <Accordion.Body>
+                  <Resume/>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </Col>
+        </Row>
+      </Container>
     </FadeIn>
     </>
   )
