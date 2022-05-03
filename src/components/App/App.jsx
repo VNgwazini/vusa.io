ed768554cfffd57d2915fb0c5f5aea13a4d303fb
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2022-05-02 23:56:14 -0700

    Added particle effect to home screen.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,101 @@
 import "./app.scss";
 // import Payment from "../payment/Payment";
 import FadeIn from 'react-fade-in';
-
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import Home from "../home/Home";
 
 export default function App() {
+  const particlesInit = async (main) => {
+    console.log(main);
 
+    await loadFull(main);
+
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return (
     <>
+        <Particles
+      style={{zIndex:'-1', position: 'absolute'}}
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#ffffff",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#0d47a1",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
     <FadeIn>
       <Home/>
       {/* <Payment/> */}
