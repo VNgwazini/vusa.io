b61eca5301848a1d5bb9d0121866e01bdf16e017
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2022-05-03 00:18:39 -0700

    Added particle effect to portfolio and experience components.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -16,9 +16,21 @@ import { useEffect, useState ,useRef } from "react"
 import Resume from "../resume/Resume";
 import Experience from "../experience/Experience";
 import Projects from "../projects/Projects";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 
 export default function Home() {
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    await loadFull(main);
+
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
 
   const textRef = useRef();
 
@@ -49,7 +61,86 @@ export default function Home() {
   const handleShowResume = () => setShowResume(true);
 
  return (
-  <Container className="home content-container py-4" id="home" fluid="true">
+   <>
+    <Particles
+      style={{zIndex:'auto', position: 'absolute'}}
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#ffffff",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#0d47a1",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+    <Container className="home content-container py-4" id="home" fluid="true">
     <FadeIn>
       <Container className="content-container py-4 px-4">
         <FadeIn>
@@ -224,5 +315,6 @@ export default function Home() {
       </Container>
     </FadeIn>
    </Container>
+   </>
   )
 }
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -6,6 +6,8 @@ import {
   Container,
 } from 'react-bootstrap/'
 import FadeIn from 'react-fade-in';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 
 import ProjectListItem from "../projectListItem/ProjectListItem";
@@ -14,11 +16,100 @@ import {
 } from "../../data";
 
 export default function Projects() {
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    await loadFull(main);
+
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
 // eslint-disable-next-line
   const [data, setData] = useState(projectData);
 
   return (
     <>
+    <Particles
+      style={{zIndex:'auto', position: 'absolute'}}
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#ffffff",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#0d47a1",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
     <Container className="projects content-container mt-4" id="projects" fluid="true">
       <FadeIn>
         <Row>
