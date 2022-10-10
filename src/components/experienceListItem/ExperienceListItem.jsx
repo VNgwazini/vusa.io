19ba2c21598677861fe2c947b057eaa2d37f8685
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2022-10-09 18:17:06 -0700

    Improved usage of whitespace.

--- src/components/experienceListItem/ExperienceListItem.jsx
+++ src/components/experienceListItem/ExperienceListItem.jsx
@@ -1,48 +1,53 @@
 import "./experienceListItem.scss";
-import { 
+import {
   Col,
   Card,
+  Row,
 } from 'react-bootstrap/'
 
-export default function ExperienceListItem({experience}) {
+export default function ExperienceListItem({ experience }) {
   return (
-      <Col className="d-flex justify-content-center py-4 experienceListItem" lg={12} md={12} sm={12}>
-        <Card className="shadow card-size">
-          <Card.Header>
-            {experience.category}
-          </Card.Header>
-          <Card.Body>
-          {Object.keys(experience.jobs).map((job, jobIndex) => (
-            <>
-            <Card.Title>
-              {experience.jobs[jobIndex].company}
-            </Card.Title>
-            <Card.Title>
-              <span style={{color: "green"}}>
-                {experience.jobs[jobIndex].position}
-              </span>
-            </Card.Title>
-            <Card.Subtitle className="mb-2" style={{color: "navy"}}>
-              {experience.jobs[jobIndex].duration}
-            </Card.Subtitle>
-            <Card.Subtitle className="mb-2 text-muted">
-              {experience.jobs[jobIndex].about}
-            </Card.Subtitle>
-            <Card.Text>
-              <ul>
-                {Object.keys(experience.jobs[jobIndex].responsibilities).map((responibility, responsibiltyIndex) => (
-                  <>
-                  <li>
-                    {experience.jobs[jobIndex].responsibilities[responsibiltyIndex]}
-                  </li>
-                  </>
-                ))}
-              </ul>
-            </Card.Text>
-            </>          
-          ))}
-          </Card.Body>
-        </Card>
-      </Col>
+    <Col className="d-flex justify-content-center py-4 experienceListItem" lg={12} md={12} sm={12}>
+      <Card className="shadow card-size">
+        <Card.Header>
+          {experience.category}
+        </Card.Header>
+        <Card.Body>
+          <Row>
+            {Object.keys(experience.jobs).map((job, jobIndex) => (
+              <>
+                <Col xl={6} lg={12} md={12} sm={12}>
+                  <Card.Title>
+                    {experience.jobs[jobIndex].company}
+                  </Card.Title>
+                  <Card.Title>
+                    <span style={{ color: "green" }}>
+                      {experience.jobs[jobIndex].position}
+                    </span>
+                  </Card.Title>
+                  <Card.Subtitle className="mb-2" style={{ color: "navy" }}>
+                    {experience.jobs[jobIndex].duration}
+                  </Card.Subtitle>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    {experience.jobs[jobIndex].about}
+                  </Card.Subtitle>
+                  <Card.Text>
+                    <ul>
+                      {Object.keys(experience.jobs[jobIndex].responsibilities).map((responibility, responsibiltyIndex) => (
+                        <>
+                          <li>
+                            {experience.jobs[jobIndex].responsibilities[responsibiltyIndex]}
+                          </li>
+                        </>
+                      ))}
+                    </ul>
+                  </Card.Text>
+                </Col>
+              </>
+            ))}
+          </Row>
+        </Card.Body>
+      </Card>
+    </Col>
   )
 }
